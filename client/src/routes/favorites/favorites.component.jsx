5cbc8ea8016ceba9ae6ac4310ddf4e07f14cab09
Victoria Benoit <vrose834@gmail.com>
2024-02-05 18:22:43 -0400

    Added login and logout icons

--- client/src/routes/favorites/favorites.component.jsx
+++ client/src/routes/favorites/favorites.component.jsx
@@ -1,6 +1,14 @@
-
+import { FavoritesContainer } from "./favorites.styles";
+import RecipeContainer from "../../components/Recipe/recipe.component";
 const FavoritesPage = () => {
-    return ( <h1>faves pages</h1> );
+    return ( 
+        <FavoritesContainer>
+            <h2>Your Favorite Recipes</h2>
+            <RecipeContainer />
+            <RecipeContainer />
+            <RecipeContainer />
+        </FavoritesContainer>
+     );
 }
  
 export default FavoritesPage;
--- client/src/routes/navigation/nav.component.jsx
+++ client/src/routes/navigation/nav.component.jsx
@@ -3,9 +3,12 @@ import { Navbar, RightNav, SearchDiv } from './nav.styles';
 import heart from '../../images/icons/icons8-heart-material-outlined/icons8-heart-24.png';
 import user from '../../images/icons/icons8-user-windows/icons8-user-32.png';
 import search from '../../images/icons/search-icon/icons8-search-24.png';
+import login from '../../images/icons/icons8-login-puffy/icons8-login-32.png';
+import logout from '../../images/icons/icons8-logout-material-filled/icons8-logout-24.png';
 import {NavLink} from 'react-router-dom';
-
+import { useState } from 'react';
 const NavBar = () => {
+    const [loggedIn, setLoggedIn] = useState(false);
     return (
         <Navbar>
             <NavLink to="/"><img src={Logo} alt="lemon lime logo" /></NavLink>
@@ -13,6 +16,10 @@ const NavBar = () => {
                 <NavLink to='search'><img src={search} alt="search" /></NavLink>
                 <NavLink to='favorites'><img src={heart} alt="favourites" /></NavLink>
                 <NavLink to='user'><img src={user} alt="user page" /></NavLink>
+                { !loggedIn ? 
+                    <NavLink to='signin' onClick={() => setLoggedIn(true)}><img src={login} alt="sign in page" /></NavLink> :
+                    <NavLink to='/' onClick={() => setLoggedIn(false)}><img src={logout} alt="logout" /></NavLink>
+                }
             </RightNav>
         </Navbar>
       );
