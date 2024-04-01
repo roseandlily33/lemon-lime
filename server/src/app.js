783063de9b4d2ec921d926d1e935c440e3386e5d
Victoria Benoit <vrose834@gmail.com>
2024-04-01 19:43:17 -0300

    trying again

--- server/src/app.js
+++ server/src/app.js
@@ -55,12 +55,19 @@ app.use('/', router);
     
 //     res.sendFile(path.join(__dirname, '..','public', 'index.html'));
 // });
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/client/build')))
-    app.get('*', (req,res) => {
+// if(process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '/client/build')))
+//     app.get('*', (req,res) => {
+//     res.send("Router is working")
+//       res.sendFile(path.join(__dirname, 'build', 'index.html'))
+//     })
+//   }
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
     res.send("Router is working")
-      res.sendFile(path.join(__dirname, 'build', 'index.html'))
-    })
   }
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 
 module.exports = app;
