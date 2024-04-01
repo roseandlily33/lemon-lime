ed47f13b50e1837149adc638ed800b508b4c260c
Victoria Benoit <vrose834@gmail.com>
2024-04-01 19:46:54 -0300

    added build

--- server/src/app.js
+++ server/src/app.js
@@ -62,10 +62,12 @@ app.use('/', router);
 //       res.sendFile(path.join(__dirname, 'build', 'index.html'))
 //     })
 //   }
+
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
     res.send("Router is working")
   }
+
   app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
