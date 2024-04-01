b5f378d5559f9e71842c45450da3c0e45ccef79d
Victoria Benoit <vrose834@gmail.com>
2024-04-01 20:14:11 -0300

    changed a couple more

--- server/src/app.js
+++ server/src/app.js
@@ -68,7 +68,7 @@ app.use('/', router);
    //res.send("Router is working")
   }
 
-  app.get('/', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 
