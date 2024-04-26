618cdbe17bd08b4958e7a6d31b30fbb7c0a7bb57
roseandlily33 <vrose834@gmail.com>
2024-04-26 15:16:10 -0300

    Created the 404 page

--- client/src/routes/notFound/notFound.component.jsx
+++ client/src/routes/notFound/notFound.component.jsx
@@ -1,6 +1,16 @@
 
+import NotFoundImage from '../../images/undraw_page_not_found_re_e9o6.svg';
+import { NavLink } from 'react-router-dom';
+
 const NotFound = () => {
-    return ( <h2>Sorry this page is not available</h2> );
+    return ( 
+        <section style={{height: '100vh', width: '100vw', 
+         display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: '1rem'}}>
+            <img src={NotFoundImage} alt="Page Not Found" />
+            <h2>Sorry this page is not available</h2>
+            <NavLink className="link" to="/" >Go Home</NavLink>
+        </section>
+     );
 }
  
 export default NotFound;
