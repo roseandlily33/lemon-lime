de68b3678a890520f8d07dbd00e4e79de84ce59c
Victoria Benoit <vrose834@gmail.com>
2024-11-25 12:14:16 -0400

    Removed http unnecessary

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-const http = require('http');
 const app = require('./app');
 require('dotenv').config();
 
@@ -6,11 +5,10 @@ const {connectToMongoose} = require('./utils/connection');
 
 const PORT = process.env.PORT || 8000;
 
-const server = http.createServer(app);
 
 async function startServer (){
     await connectToMongoose();
-    await server.listen(PORT, () => {
+    await app.listen(PORT, () => {
         console.log('Listening on port mongoose', PORT)
     });
 }
